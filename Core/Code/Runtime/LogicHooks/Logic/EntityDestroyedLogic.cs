162b7e49f965a05a7a7e11cd09e6b3e9842ee1f3
Danon5 <danonfive@gmail.com>
2023-07-30 18:07:51 -0700

    - Added basic Preserve attribute check for LogicHooks

--- Core/Code/Runtime/LogicHooks/Logic/EntityDestroyedLogic.cs
+++ Core/Code/Runtime/LogicHooks/Logic/EntityDestroyedLogic.cs
@@ -1,7 +1,9 @@
 ﻿using UFlow.Addon.ECS.Core.Runtime;
 using UFlow.Core.Runtime;
+using UnityEngine.Scripting;
 
 namespace UFlow.Addon.SaveState.Core.Runtime {
+    [Preserve]
     public struct EntityDestroyedLogic : ILogic<EntityDestroyedHook> {
         public void Execute(in EntityDestroyedHook hook) {
             if (Stashes<InstantiatedSceneEntity>.TryGet(hook.worldId, out var stash) && stash.Has(hook.entity.id))
--- Core/Code/Runtime/LogicHooks/Logic/PrefabSceneEntityCreatedLogic.cs
+++ Core/Code/Runtime/LogicHooks/Logic/PrefabSceneEntityCreatedLogic.cs
@@ -1,7 +1,9 @@
-﻿using UFlow.Addon.ECS.Core.Runtime;
+﻿using Rewired.Utils.Attributes;
+using UFlow.Addon.ECS.Core.Runtime;
 using UFlow.Core.Runtime;
 
 namespace UFlow.Addon.SaveState.Core.Runtime {
+    [Preserve]
     public struct PrefabSceneEntityCreatedLogic : ILogic<PrefabSceneEntityCreatedHook> {
         public void Execute(in PrefabSceneEntityCreatedHook hook) {
             hook.sceneEntity.Entity.Set(new InstantiatedSceneEntity {
